b47723b615d62a6624d31b7bc2ced3ef34274f45
ShoutTree <zengxiaoqi@gmail.com>
2017-05-27 01:08:16 +0800

    fixed some bug

--- concatMediaFiles.py
+++ concatMediaFiles.py
@@ -4,7 +4,7 @@ import os,sys
 LIST_FILE_NAME = "temp_mylist.txt"
 ERROR_FILE_NAME = "error.txt"
 FILE_EXT = ".mp4"
-f_error = null
+global f_error
 
 def generateListFile(dir):
 	global f_error
@@ -23,6 +23,7 @@ def generateListFile(dir):
 
 					t=os.popen('ffmpeg -v error -i ' + '\"' + minuteFilePath + '\"' + ' -f null - 2>&1').read()
 					if (t.find('atom not found') >= 0):
+						print 'file cannot open: ' + minuteFilePath
 						f_error.write('file cannot open: ' + minuteFilePath + "\n")
 					else:
 						f_list_file.write("file " + "\'" + minuteFilePath + "\'" + "\n")
@@ -46,11 +47,13 @@ def concatMediaFiles(dataStr):
 # -------------- main, call from bat or shell script --------------
 if __name__ == '__main__':
 	if len(sys.argv) >= 2:
-
-		f_error = fopen(ERROR_FILE_NAME, "w+")
+		global f_error
+		f_error = open(ERROR_FILE_NAME, "w+")
 
 		dataRootPath = sys.argv[1]
 		for dateToConcat in os.listdir(dataRootPath):
+			# if dateToConcat == '2017-05-22':
+			# 	continue
 			dateFolderPath = dataRootPath + "/" + dateToConcat
 			generateListFile(dateFolderPath)
 			concatMediaFiles(dateToConcat)
